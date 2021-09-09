7018533f5c5f94ce5a722a083a1b3683533173f0
VitorMob <69119427+VitorMob@users.noreply.github.com>
2021-09-09 19:12:25 -0300

    release memory
    added `this` to class access

--- src/include/stack.cpp
+++ src/include/stack.cpp
@@ -4,19 +4,19 @@ void __stack::__MallocMemory(Word_t __tam)
 {
     if (__tam <= 0)
         exit(1);
-    __stack::__Memory = (Word_t *)malloc(__tam);
-    if (!__stack::__Memory)
+    this->__Memory = new Word_t[__tam];
+    if (!this->__Memory)
         exit(2);
 }
 
 void __stack::__ValidMemory()
 {
-    if (__stack::PC > __stack::Buffer)
+    if (this->PC > this->Buffer)
     {
         std::cout << "Stack overflow" << std::endl;
         exit(3);
     }
-    if(__stack::PC == 0)
+    if(this->PC == 0)
     {
         std::cout << "Stack empty" << std::endl;
         exit(4);
@@ -25,49 +25,49 @@ void __stack::__ValidMemory()
 
 void __stack::__WriteMemory(Word_t __addr, Word_t __dice)
 {
-    __stack::__Memory[__addr] = __dice;
+    this->__Memory[__addr] = __dice;
 }
 
 Word_t __stack::__ReadMemory(Word_t __addr)
 {
-    return __stack::__Memory[__addr];
+    return this->__Memory[__addr];
 }
 
 void __stack::stack_push(Word_t __dice)
 {
-    __stack::SP -= 2;
-    __stack::BS -= 2;
-    __stack::__WriteMemory(__stack::SP, __dice & 0xff);
-    __stack::__WriteMemory(__stack::BS, (__dice >> 8) & 0x1);
-    __stack::PC += 2;
-    __stack::__ValidMemory();
+    this->SP -= 2;
+    this->BS -= 2;
+    this->__WriteMemory(this->SP, __dice & 0xff);
+    this->__WriteMemory(this->BS, (__dice >> 8) & 0x1);
+    this->PC += 2;
+    this->__ValidMemory();
 }
 
 Word_t __stack::stack_pop()
 {
-    __stack::__ValidMemory();
-    Word_t Read = __stack::__ReadMemory(__stack::SP);
-    __stack::SP += 2;
+    this->__ValidMemory();
+    Word_t Read = this->__ReadMemory(this->SP);
+    this->SP += 2;
     return Read;
 }
 
 Word_t __stack::stack_size()
 {
-    __stack::__ValidMemory();
-    return __stack::PC-=1;
+    this->__ValidMemory();
+    return this->PC-=1;
 }
 
 Word_t __stack::stack_sign()
 {    
-    __stack::__ValidMemory();
-    Word_t Read = __stack::__ReadMemory(__stack::BS);
-    __stack::BS += 2;
+    this->__ValidMemory();
+    Word_t Read = this->__ReadMemory(this->BS);
+    this->BS += 2;
     return Read;
 }
 
 bool __stack::stack_empty()
 {
-    if(__stack::PC == 0)
+    if(this->PC == 0)
         return true;
     else 
         return false;
@@ -75,13 +75,14 @@ bool __stack::stack_empty()
 
 __stack::Stack(Word_t __tam)
 {
-    __stack::Buffer = __tam;
-    __stack::SP = 0x2;
-    __stack::PC = 0x00;
-    __stack::BS = 1;
-    __stack::__MallocMemory(__stack::Buffer);
+    this->Buffer = __tam;
+    this->SP = 0x02;
+    this->PC = 0x00;
+    this->BS = 0x01;
+    this->__MallocMemory(this->Buffer);
 }
 
 __stack::~Stack()
 {
+    delete[] this->__Memory;
 }
